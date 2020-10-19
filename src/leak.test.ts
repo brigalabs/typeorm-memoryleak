ff8c2a85d041a44e6a053e95a29def82bbd48401
Brice Leroy <brice@brigalabs.com>
2020-10-19 11:08:28 -0700

    Use Connection as recommended

--- src/leak.test.ts
+++ src/leak.test.ts
@@ -1,8 +1,21 @@
-import { createConnection } from "typeorm";
+import { Connection } from "typeorm";
+// import { createConnection } from "typeorm";
 
-describe("leak detector", () => {
-  it("test leak", async () => {
-    let conn = await createConnection({
+describe("typeorm conn leak detector", () => {
+  //   it("test createConnection leak", async () => {
+  //     const conn: Connection | null = await createConnection({
+  //       name: "default",
+  //       type: "sqlite",
+  //       database: ":memory:",
+  //       logging: false,
+  //       entities: [],
+  //     });
+
+  //     await conn.close();
+  //   });
+
+  it("test Connection leak", async () => {
+    const conn: Connection | null = new Connection({
       name: "default",
       type: "sqlite",
       database: ":memory:",
