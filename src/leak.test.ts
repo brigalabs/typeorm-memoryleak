27786f3d48275932d28d164c13145fa5c3604b5b
Brice Leroy <brice@brigalabs.com>
2021-07-12 09:48:05 -0700

    remove commented code

--- src/leak.test.ts
+++ src/leak.test.ts
@@ -1,19 +1,6 @@
 import { Connection } from "typeorm";
-// import { createConnection } from "typeorm";
 
 describe("typeorm conn leak detector", () => {
-  //   it("test createConnection leak", async () => {
-  //     const conn: Connection | null = await createConnection({
-  //       name: "default",
-  //       type: "sqlite",
-  //       database: ":memory:",
-  //       logging: false,
-  //       entities: [],
-  //     });
-
-  //     await conn.close();
-  //   });
-
   it("test Connection leak", async () => {
     const conn: Connection | null = new Connection({
       name: "default",
