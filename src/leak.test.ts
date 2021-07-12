7bb126b11f3f6f756b84b5144320fb588212da58
Brice Leroy <brice@brigalabs.com>
2021-07-12 10:34:31 -0700

    connect prior to close

--- src/leak.test.ts
+++ src/leak.test.ts
@@ -2,13 +2,15 @@ import { Connection } from "typeorm";
 
 describe("typeorm conn leak detector", () => {
   it("test Connection leak", async () => {
-    const conn: Connection | null = new Connection({
+    let conn: Connection | null = new Connection({
       name: "default",
       type: "sqlite",
       database: ":memory:",
       logging: false,
       entities: [],
     });
+
+    await conn.connect();
 
     await conn.close();
   });
